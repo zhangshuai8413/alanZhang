96e309eafe2575439f1b83447200b2c924c64e36
alan.zhang <alan.zhang@kakaocorp.com>
2016-03-18 14:18:47 +0800

    git 练习

---
+++ gitExample/gitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitExample
+//
+//  Created by kakao on 16/3/18.
+//  Copyright © 2016年 shuai zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
